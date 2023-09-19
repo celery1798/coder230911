04643076e7cafd5c0550488bb39af91da6da5f3f
Parallels <celery1798@163.com>
2023-09-19 14:02:26 +0800

    aa

---
+++ few/fork.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	pid_t pid;
+
+	printf("[%d]Begin!",getpid());
+	
+	fflush(NULL);
+
+	pid = fork();
+	if(pid < 0)
+	{
+		perror("fork()");
+		exit(1);
+	}
+	if(pid > 0)		// parent
+	{
+		printf("[%d]Parent is working\n",getpid());
+	}
+	else			//child
+	{
+		printf("[%d]Child is working\n",getpid());
+	}
+
+	printf("[%d]End!\n",getpid());
+
+//	sleep(1000);
+
+	exit(0);
+}
+
+
+
